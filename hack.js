b8bcfa7d38b49419a533fd0b0e9c7a4eb2ba14f5
HarishG03 <131017755+HarishG03@users.noreply.github.com>
2024-03-17 01:18:28 +0530

    Update2 hack.js

--- hack.js
+++ hack.js
@@ -7,7 +7,7 @@ window.addEventListener("load", function() {
 });
 const addEventOnElements = function(elements, eventType, callback) {
     for (let i = 0, len = elements.length; i < len; i++) {
-        elements[i].addEventListener(eventType, callback);
+        elements[i].addEventListener(eventType,callback);
     }
 }
 const navbar = document.querySelector("[data-navbar]");
